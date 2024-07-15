0151b6a4a31e44162e1719eb19233510abd6cb78
reagan02 <r.mabawad.529147@umindanao.edu.ph>
2024-07-15 11:34:56 +0800

    update model

--- src/controllers/form.controller.js
+++ src/controllers/form.controller.js
@@ -1,74 +1,74 @@
 import { Component } from "../models/component.model.js";
 import FormComponent from "../objects/component.js";
-import FormObject from "../objects/Form.js";
+import FormObject from "../objects/form.js";
 import Form from "../models/form.models.js";
 const get = async (req, res) => {
-  res.redirect("/create");
+	res.redirect("/create");
 };
 
 const post = async (req, res) => {
-  console.log(req.body);
-  console.log("hello");
-  res.send(req.body);
+	console.log("hello");
+	res.send(req.body);
 };
 
 const createForm = async (req, res) => {
-  res.render("pages/create", { layout: "./layouts/main" });
+	res.render("pages/create", { layout: "./layouts/main" });
 };
 
 const submitForm = async (req, res) => {
-  try {
-    // Assuming req.body contains the JSON data sent via fetch
-    const formData = req.body;
+	try {
+		// Assuming req.body contains the JSON data sent via fetch
+		const formData = req.body;
+		console.log("Submitting form data:", formData);
+		const components = [];
 
-    console.log(formData);
+		formData.formComponents.forEach((component) => {
+			const formComponent = new FormComponent(component);
+			const newComponent = new Component(formComponent.toCreateFormModel());
+			components.push(newComponent);
+		});
 
-    const components = [];
-    formData.formComponents.forEach((component) => {
-      const formComponent = new FormComponent(component);
-      const newComponent = new Component(formComponent.toCreateFormModel());
-      components.push(newComponent);
-    });
+		const form = new FormObject({
+			name: formData.formName,
+			description: formData.formDescription,
+			components: components,
+		});
 
-    const form = new FormObject({
-      name: formData.formName,
-      description: formData.formDescription,
-      components: components,
-    });
-
-    new Form(form.toCreateFormModel()).save();
-
-    return res.json({ form });
-    res.render("pages/create", { data: formData });
-  } catch (error) {
-    console.error("Error processing form:", error);
-    res.status(500).send("Error processing form");
-  }
+		new Form(form.toCreateFormModel()).save();
+		console.log(a);
+		return res.json({ form });
+	} catch (error) {
+		console.error("Error processing form:", error);
+		res.status(500).send("Error processing form");
+	}
 };
 
 const editForm = async (req, res) => {
-  const data = {};
+	const data = {};
 
-  res.render("pages/create", { data });
+	res.render("pages/create", { data });
 };
 ``;
 
 const listForm = async (req, res) => {
-  const data = {};
-  res.render("pages/listform");
+	const data = {};
+	res.render("pages/listform");
 };
 
 const viewForm = async (req, res) => {
-  const data = {};
-  res.render("pages/viewform");
+	const data = {};
+
+	cons;
+
+	res.render("pages/viewform");
 };
 
 export default {
-  get,
-  post,
-  createForm,
-  submitForm,
-  listForm,
-  editForm,
-  viewForm,
+	get,
+	post,
+	createForm,
+	submitForm,
+	listForm,
+	editForm,
+	viewForm,
 };

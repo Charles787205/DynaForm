e423e10b2cc907a6a6f69e45f33f79018a94fd83
kruizo <kynethruizo@gmail.com>
2024-07-22 13:00:35 +0800

    added new changes

--- public/js/editSave.js
+++ public/js/editSave.js
@@ -1,10 +1,23 @@
 var delay;
 const handler = (event) => {
-   clearTimeout(delay);
+  const loader = document.getElementById("change-indicator");
+  const loader_text = document.getElementById("change-text");
+  const indicator = document.getElementById("change"); 
+  loader.style.display = "flex";
+  loader_text.innerHTML = "Saving";
+  indicator.style.display = "flex";
+
+  clearTimeout(delay);
     delay =  setTimeout(() => {
+
       console.log("Saved");
+
+
       updateForm();      
+      loader_text.innerHTML = "All changes saved.";
+      loader.style.display = "none";
     }, 500);
+ 
 };
 document.addEventListener("drop", handler);
 document.addEventListener("input", handler);
